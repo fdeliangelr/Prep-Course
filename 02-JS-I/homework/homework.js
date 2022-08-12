ef2ce6834bba41beb6bd66f8508aef9182c0e3db
fdeliangelr <fdeliangel.rueda@gmail.com>
2022-08-12 19:37:24 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,13 +4,13 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = "hola";
 
-// Crea una variable booleana:
+// Crea una variable leana:
 const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
